a7a38061a209216999d77b057b38458cd19c7617
Alexander Shestakov <Maksar.mail@gmail.com>
2012-02-19 00:12:32 +0300

    Adding more complex examples.

--- lib/equation_builder.rb
+++ lib/equation_builder.rb
@@ -8,8 +8,10 @@ class EquationBuilder
 
   def solve
     @numbers.permutation do |numbers|
-      expression = numbers.join(@operators.first)
-      return expression if eval(expression) == @result
+      @operators.repeated_permutation(@numbers.length - 1) do |operators|
+        expression = numbers.zip(operators).flatten.join
+        return expression if eval(expression) == @result
+      end
     end
     ''
   end
--- spec/equation_builder_spec.rb
+++ spec/equation_builder_spec.rb
@@ -13,5 +13,25 @@ describe EquationBuilder do
       subject { EquationBuilder.new([2, 4], -2, ['-']) }
       its(:solve) { should == '2-4' }
     end
+
+    context 'with multiple operators' do
+      subject { EquationBuilder.new([2, 4, 6], 4, ['-', '+']) }
+      its(:solve) { should == '2-4+6' }
+    end
+
+    context 'with repeating operator' do
+      subject { EquationBuilder.new([2, 4, 6], 12, ['+']) }
+      its(:solve) { should == '2+4+6' }
+    end
+
+    context 'with multiple repeating operators' do
+      subject { EquationBuilder.new([2, 4, 6, 8], 0, ['+', '-']) }
+      its(:solve) { should == '2-4-6+8' }
+    end
+  end
+
+  context 'equation not found' do
+    subject { EquationBuilder.new([1, 2], 4, ['+']) }
+    its(:solve) { should == '' }
   end
 end
