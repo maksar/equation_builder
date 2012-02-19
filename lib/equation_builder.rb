bcf392fde66a30884bc068133a01f1de4059a672
Alexander Shestakov <Maksar.mail@gmail.com>
2012-02-19 04:13:28 +0300

    Finishing implementation.

--- lib/equation_builder.rb
+++ lib/equation_builder.rb
@@ -1,18 +1,65 @@
 class EquationBuilder
 
-  def initialize numbers, result, operators
-    @numbers = numbers
-    @result = result
-    @operators = operators
+  def initialize _numbers, _result, _operators
+    @numbers = _numbers
+    @result = _result
+    @operators = Array(_operators)
+
+    @matcher = proc { |numbers, operators, result| numbers.zip(operators).flatten.join if eval(make_float(numbers).zip(operators).flatten.join) == result }
   end
 
+
   def solve
-    @numbers.permutation do |numbers|
-      @operators.repeated_permutation(@numbers.length - 1) do |operators|
-        expression = numbers.map(&:to_f).zip(operators).flatten.join
-        return numbers.zip(operators).flatten.join if eval(expression) == @result
+    solve_simple || solve_with_parentheses || nil
+  end
+
+  private
+
+  def solve_simple
+    with_permutations do |numbers, operators|
+      result = @matcher.call(numbers, operators, @result)
+      return result if result
+    end
+    nil
+  end
+
+  def solve_with_parentheses
+    with_permutations do |numbers, operators|
+      with_parentheses(@numbers.length.div(2), numbers, (0..@numbers.length).to_a) do |numbers_with_parentheses|
+        result = @matcher.call(numbers_with_parentheses, operators, @result)
+        return result if result
       end
     end
-    ''
+    nil
+  end
+
+  def with_parentheses number, numbers, parentheses_options, &block
+    return if number == 0
+
+    parentheses_options.combination(2) do |positions|
+      block.call add_parentheses numbers.dup, positions
+    end
+
+    parentheses_options.combination(2) do |positions|
+      with_parentheses number - 1, add_parentheses(numbers.dup, positions), parentheses_options, &block
+    end
+  end
+
+  def with_permutations &block
+    @numbers.permutation do |numbers|
+      @operators.repeated_permutation @numbers.length - 1 do |operators|
+        block.call(numbers, operators)
+      end
+    end
+  end
+
+  def make_float numbers
+    numbers.map { |element| "#{element}*1.0" }
+  end
+
+  def add_parentheses numbers, position
+    numbers[position.first] = "(#{numbers[position.first]}"
+    numbers[position.last] = "#{numbers[position.last]})"
+    numbers
   end
 end
--- spec/equation_builder_spec.rb
+++ spec/equation_builder_spec.rb
@@ -5,43 +5,60 @@ require 'equation_builder'
 describe EquationBuilder do
   context 'simple equations' do
     context 'with + operator' do
-      subject { EquationBuilder.new([2, 4], 6, ['+']) }
+      subject { EquationBuilder.new([2, 4], 6, '+') }
       its(:solve) { should == '2+4' }
     end
 
     context 'with - operator' do
-      subject { EquationBuilder.new([2, 4], -2, ['-']) }
+      subject { EquationBuilder.new([2, 4], -2, '-') }
       its(:solve) { should == '2-4' }
     end
 
     context 'with multiple operators' do
-      subject { EquationBuilder.new([2, 4, 6], 4, ['-', '+']) }
+      subject { EquationBuilder.new([2, 4, 6], 4, %w(- +)) }
       its(:solve) { should == '2-4+6' }
     end
 
     context 'with repeating operator' do
-      subject { EquationBuilder.new([2, 4, 6], 12, ['+']) }
+      subject { EquationBuilder.new([2, 4, 6], 12, '+') }
       its(:solve) { should == '2+4+6' }
     end
 
     context 'with multiple repeating operators' do
-      subject { EquationBuilder.new([2, 4, 6, 8], 0, ['+', '-']) }
+      subject { EquationBuilder.new([2, 4, 6, 8], 0, %w(+ -)) }
       its(:solve) { should == '2-4-6+8' }
     end
   end
 
   context 'with long equation' do
-    subject { EquationBuilder.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, ['*', '-']) }
+    subject { EquationBuilder.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, %w(* -)) }
     its(:solve) { should == '1*2*3*4*5-6*9-7*8-10' }
   end
 
   context 'with float numbers' do
-    subject { EquationBuilder.new([1, 2], 0.5, ['/', '*']) }
+    subject { EquationBuilder.new([1, 2], 0.5, %w(/ *)) }
     its(:solve) { should == '1/2' }
   end
 
+  context 'with parentheses' do
+    context 'with single parentheses' do
+      subject { EquationBuilder.new([1, 2, 4, 5], 17, %w(+ *)) }
+      its(:solve) { should == '(1+2)*4+5' }
+    end
+
+    context 'with double parentheses' do
+      subject { EquationBuilder.new([5, 5, 5, 5], 100, %w(+ *)) }
+      its(:solve) { should == '(5+5)*(5+5)' }
+    end
+  end
+
+  context 'ultimate example' do
+    subject { EquationBuilder.new([1, 3, 4, 6], 24, %w(- /)) }
+    its(:solve) { should == '6/(1-3/4)' }
+  end
+
   context 'equation not found' do
-    subject { EquationBuilder.new([1, 2], 4, ['+']) }
-    its(:solve) { should == '' }
+    subject { EquationBuilder.new([1, 2], 4, '+') }
+    its(:solve) { should be_nil }
   end
 end
